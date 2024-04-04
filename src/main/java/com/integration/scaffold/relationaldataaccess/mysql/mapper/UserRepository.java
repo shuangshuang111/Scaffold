53ab33db9cbf32ba95ba400bb8e3fd928a089d1d
shuangshuang111 <2446009150@qq.com>
2024-04-04 17:54:24 +0800

    去掉无用的mybatis

--- src/main/java/com/integration/scaffold/relationaldataaccess/mysql/mapper/UserRepository.java
+++ src/main/java/com/integration/scaffold/relationaldataaccess/mysql/mapper/UserRepository.java
@@ -1,9 +1,10 @@
 package com.integration.scaffold.relationaldataaccess.mysql.mapper;
 
 import com.integration.scaffold.relationaldataaccess.mysql.entity.User;
-import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 
-public interface UserRepository extends JpaRepositoryImplementation<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
 
 }
